69e119e6883a94ff64452babe9ba98b5ad97a7f5
Yash Gaur <yashgaur2525@gmail.com>
2022-11-13 16:54:35 -0500

    improved structuring of project

---
+++ login page/login.js
@@ -0,0 +1,6 @@
+// const loginBtn = document.getElementById('login');
+
+// loginBtn.addEventListener('click', function(){
+//     console.log('login btn');
+//     location.href = '../TEMPorSCRATCH/templateScratch.html';
+// });
